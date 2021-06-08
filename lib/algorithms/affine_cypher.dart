a78b852688a5325187d4e8fccf4c4d738331a685
aiman7407 <46803625+aiman7407@users.noreply.github.com>
2021-06-08 18:39:01 +0200

    algorithms

---
+++ lib/algorithms/affine_cypher.dart
@@ -0,0 +1,49 @@
+
+
+import 'package:security_project/algorithms/utils.dart';
+
+/// Affine cipher implementation.
+class AffineCipher {
+  late int key, m;
+
+  /// [a] and [b] are required.
+  ///
+  /// [ArgumentError] if [a] is not prime with 26.
+  AffineCipher(int a, int b) {
+    if (a.gcd(26) != 1) {
+      throw ArgumentError("$a is not prime with 26.");
+    }
+    this.key = a;
+    this.m = b;
+  }
+
+  String _convert(String text, String m) {
+    StringBuffer cipher = StringBuffer();
+
+    for (var i = 0; i < text.length; i++) {
+      String ch = text[i];
+
+      if (isLetter(ch)) {
+        String s;
+
+        if (m == "encrypt") {
+          s = alphabet[((this.key * alphabet.indexOf(ch.toUpperCase())) + this.m) % 26];
+        } else {
+          s = alphabet[(this.key.modInverse(26) * (alphabet.indexOf(ch.toUpperCase()) - this.m)) % 26];
+        }
+
+        cipher.write(isUpper(ch) ? s.toUpperCase() : s.toLowerCase());
+      } else {
+        cipher.write(ch);
+      }
+    }
+
+    return cipher.toString();
+  }
+
+  /// Encrypt [text].
+  String encrypt(String text) => _convert(text, "encrypt");
+
+  /// Decrypt [text].
+  String decrypt(String text) => _convert(text, "decrypt");
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,38 @@
-import 'package:flutter/material.dart';
-import 'package:security_project/src/app_root.dart';
+import 'package:security_project/algorithms/affine_cypher.dart';
+import 'package:security_project/algorithms/ceaser_cipher.dart';
+import 'package:security_project/algorithms/rot5cipher.dart';
+import 'package:security_project/algorithms/vignere.dart';
+
 
 void main() {
-  runApp(AppRoot());
+
+  var affineCypher=AffineCipher(3, 5);
+  String encrypt = affineCypher.encrypt('Hello World!');
+  String decrypt = affineCypher.decrypt(encrypt);
+   print(encrypt); // Armmv Tvemo!
+   print(decrypt); // Hello World!
+
+  var eslamVigner=FallVigenereCipher('51327');
+  String eslamEncrypt = eslamVigner.encrypt('vigenere cipher');
+  String eslamDecrypt = eslamVigner.decrypt(eslamEncrypt);
+  print(eslamEncrypt); // Lwwla Agclp!
+  print(eslamDecrypt); // vigenere cipher
+
+  var gendyRot=Rot5();
+  String gendyEncrypt = gendyRot.encrypt('1 23 456 7890');
+  String gendyDecrypt = gendyRot.decrypt(gendyEncrypt);
+  print(gendyEncrypt); // 6 78 901 2345
+  print(gendyDecrypt); // 1 23 456 7890
+
+
+  var dohaCihper=CaesarCipher(7);
+  String dohaEncrypt = dohaCihper.encrypt('eslam ludo');
+  String dohaDycrypt = dohaCihper.decrypt(dohaEncrypt);
+  print(dohaEncrypt); // 6 78 901 2345
+  print(dohaDycrypt); // 1 23 456 7890
+
+
+
+
+
 }
